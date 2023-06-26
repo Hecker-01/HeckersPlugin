b917d98fcbe04aa68c666aed7bed7d6111b30c3b
Hecker_01 <jesseflantua@icloud.com>
2023-06-26 12:57:17 +0200

    main

--- src/main/java/net/heckerdev/testplugin/TestPlugin.java
+++ src/main/java/net/heckerdev/testplugin/TestPlugin.java
@@ -2,9 +2,6 @@ package net.heckerdev.testplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import static org.bukkit.Bukkit.getLogger;
-
-
 public final class TestPlugin extends JavaPlugin {
 
     @Override
@@ -12,7 +9,7 @@ public final class TestPlugin extends JavaPlugin {
         // Plugin startup logic
         // Register our command "kit" (set an instance of your command class as executor)
         this.getCommand("kit").setExecutor(new CommandKit());
-        this.getCommand("book").setExecutor(new CommandKit());
+        this.getCommand("book").setExecutor(new CommandBook());
         getLogger().info("Successfully loaded commands!");
     }
 
