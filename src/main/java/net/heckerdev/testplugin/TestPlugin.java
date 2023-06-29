4b5ff65cb89b631658fc02f5adc950f95db2004f
jesse <jesseflantua@icloud.com>
2023-06-29 09:48:41 +0200

    Now uses booleans

--- src/main/java/net/heckerdev/testplugin/TestPlugin.java
+++ src/main/java/net/heckerdev/testplugin/TestPlugin.java
@@ -2,9 +2,6 @@ package net.heckerdev.testplugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Objects;
-
 public final class TestPlugin extends JavaPlugin {
 
     @Override
@@ -17,22 +14,11 @@ public final class TestPlugin extends JavaPlugin {
 
         // registering listeners.
 
-        String placeMessage = getConfig().getString("Listeners.PlaceMessage");
-        if (Objects.equals(placeMessage, "true")) {Bukkit.getPluginManager().registerEvents(new BlockPlaceEventListener(), this);
-        } else if (!Objects.equals(placeMessage, "false")) {
-            getLogger().warning("Error in config.yml: Listener.PlaceMessage wasn't a boolean, reverting to true...");
-            Bukkit.getPluginManager().registerEvents(new BlockPlaceEventListener(), this);
-            getConfig().set("Listeners.PlaceMessage", true);
-            saveConfig();
-        }
-        String breakMessage = getConfig().getString("Listeners.BreakMessage");
-        if (Objects.equals(breakMessage, "true")) {Bukkit.getPluginManager().registerEvents(new BlockBreakEventListener(), this);
-        } else if (!Objects.equals(breakMessage, "false")) {
-            getLogger().warning("Error in config.yml: Listener.BreakMessage wasn't a boolean, reverting to true...");
-            Bukkit.getPluginManager().registerEvents(new BlockBreakEventListener(), this);
-            getConfig().set("Listeners.BreakMessage", true);
-            saveConfig();
-        }
+        boolean placeMessage = getConfig().getBoolean("Listeners.PlaceMessage");
+        if (placeMessage) {Bukkit.getPluginManager().registerEvents(new BlockPlaceEventListener(), this);}
+        boolean breakMessage = getConfig().getBoolean("Listeners.BreakMessage");
+        if (breakMessage) {Bukkit.getPluginManager().registerEvents(new BlockBreakEventListener(), this);}
+
         // Loaded message.
         getLogger().info("Successfully loaded TestPlugin!");
     }
